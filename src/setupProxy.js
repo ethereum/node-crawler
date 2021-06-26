ad3cdaf35363b027c06259cd6a21a2209ab0977d
Mohamed Mansour <hello@mohamedmansour.com>
2021-06-26 00:38:25 +0000

    Cache API to once every minute

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,26 @@
-const createProxyMiddleware = require('http-proxy-middleware');
+const cron = require('node-cron');
+const fetch = require('node-fetch');
+
+let clients;
+
+async function cacheApiCall() {
+  const response = await fetch('https://crawler.tmio.io/rest/clients');
+  clients = await response.json()
+}
+
+cacheApiCall()
+
+cron.schedule('* * * * *', async () => {
+  await cacheApiCall() 
+});
 
 module.exports = function(app) {
-  app.use(
-    '/rest',
-    createProxyMiddleware({
-      target: 'https://crawler.tmio.io',
-      changeOrigin: true,
-    })
-  );
-};
+  app.get('/rest/clients', (req, res) => {
+    if (!clients) {
+      return res.json({
+        status: 'loading'
+      })
+    }
+    return res.json(clients)
+  }
+)};
