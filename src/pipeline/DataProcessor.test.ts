58f2101d0142a17a696b188edb6f6acee344c050
Mohamed Mansour <hello@mohamedmansour.com>
2021-06-26 23:53:23 +0000

    Add more data processing tests

--- src/pipeline/DataProcessor.test.ts
+++ src/pipeline/DataProcessor.test.ts
@@ -4,6 +4,8 @@ test("processes full schema correctly", () => {
   const peers = [
     { clientId: "Geth/v1.10.3-stable-991384a7/linux-amd64/go1.16.3", count: 2 },
     { clientId: "Geth/v1.10.4-stable/linux-amd64/go1.16.4", count: 1 },
+    { clientId: "Geth/goerli/v1.10.4-unstable-966ee3ae-20210528/linux-amd64/go1.16.4", count: 1 },
+    { clientId: "Geth/c30khkq4no1is608bj70/v1.10.4-stable-aa637fd3/linux-amd64/go1.16.5", count: 1 },
   ];
    
   const errorCallback = jest.fn()
@@ -12,7 +14,7 @@ test("processes full schema correctly", () => {
   expect(errorCallback).not.toBeCalled()
 
   expect(processor[0]).toEqual({
-    id: 'Geth',
+    name: 'Geth',
     version: { 
       major: 1,
       minor: 10,
@@ -35,12 +37,37 @@ test("processes full schema correctly", () => {
   })
 
   expect(processor[1]).toEqual({
-    id: 'Geth',
+    name: 'Geth',
     version: { 
       major: 1,
       minor: 10,
       patch: 4,
       tag: 'stable'
+    },
+    os: {
+      vendor: 'linux',
+      architecture: 'amd64'
+    },
+    runtime: {
+      name: 'go',
+      version: { 
+        major: 1,
+        minor: 16,
+        patch: 4
+      }
+    }
+  })
+
+  expect(processor[2]).toEqual({
+    name: 'Geth',
+    label: 'goerli',
+    version: { 
+      major: 1,
+      minor: 10,
+      patch: 4,
+      tag: 'unstable',
+      build: '966ee3ae',
+      date: '20210528'
     },
     os: {
       vendor: 'linux',
@@ -72,6 +99,25 @@ test("processes runtimes correctly", () => {
     { name: 'java', version: { major: 11 } },
     { name: 'go', version: { major: 1, minor: 16, patch: 5 } },
     { name: 'rustc', version: { major: 1, minor: 52, patch: 1 } }
+  ])
+});
+
+test("processes os correctly", () => {
+  const peers = [
+    { clientId: "besu/v21.7.0-RC1/linux-amd64/corretto-java-11", count: 14 },
+    { clientId: "erigon/v2021.06.5-alpha-a0694dd3/windows/go1.16.5", count: 1 },
+    { clientId: "OpenEthereum/v3.2.6-stable-f9f4926-20210514/x86_64-linux-gnu/rustc1.52.1", count: 1 },
+  ];
+   
+  const errorCallback = jest.fn()
+  const processor = ClientsProcessor(peers, errorCallback)
+  expect(processor).toHaveLength(peers.length)
+  expect(errorCallback).not.toBeCalled()
+  console.log(processor.map(p => (p?.os)))
+  expect(processor.map(p => (p?.os))).toEqual([
+    { vendor: 'linux', architecture: 'amd64' },
+    { vendor: 'windows' },
+    { vendor: 'linux', architecture: 'x86_64' },
   ])
 });
 
--- src/pipeline/DataProcessor.ts
+++ src/pipeline/DataProcessor.ts
@@ -24,141 +24,155 @@ export interface OperatingSytem {
   architecture: string;
 }
 
-export interface ClientDetail {
+export interface Client {
   label?: string;
   version?: Version;
   os?: OperatingSytem;
   runtime?: Runtime;
 }
 
+interface ParseParam {
+  primaryKey: number
+  errorCallback: (entity: string, data: string, primaryKey: number) => void
+}
+
+const osMapping: { [key: string]: string } = {
+  linux: "vendor",
+  windows: "vendor",
+  darwin: "vendor",
+  x86_64: "architecture",
+  x64: "architecture",
+  amd64: "architecture"
+};
+
+function tryParseNumber(input: string): number | undefined  {
+  return parseInt(input) || undefined
+}
+
+function parseVersion(version: string, parseOpt: ParseParam): Version | undefined {
+  const matches = version.match(/v?(?<major>\d+)(?:.(?<minor>\d+).(?<patch>\d+)(?:-(?<tag>\w+)(?:-(?<build>[a-zA-Z0-9]+)(?:-(?<date>\d+))?)?)?)?/)
+  
+  if (!matches?.groups) {
+    parseOpt.errorCallback('version', version, parseOpt.primaryKey);
+    return undefined
+  }
+
+  const minor = tryParseNumber(matches.groups.minor)
+  const patch = tryParseNumber(matches.groups.patch)
+  const tag = matches.groups.tag
+  const build = matches.groups.build
+  const date = matches.groups.date
+
+  return {
+    major: parseInt(matches.groups.major),
+    ...minor && { minor },
+    ...patch && { patch },
+    ...tag && { tag },
+    ...build && { build },
+    ...date && { date },
+  }
+}
+
+function parseOs(os: string, parseOpt: ParseParam): OperatingSytem | undefined {
+  const match = matchAll(os, /(linux|windows|darwin|x86_64|x64|amd64)/g)
+  const matches = Array.from(match);
+
+  if (matches.length) {
+    const result: any = {};
+    matches.forEach((match) => {
+      const value = match[1];
+      const key = osMapping[value];
+      result[key] = value;
+    });
+
+    return result;
+  }
+
+  parseOpt.errorCallback('os', os, parseOpt.primaryKey);
+  return undefined;
+}
+
+function parseRuntime(runtime: string, parseOpt: ParseParam): Runtime | undefined {
+  const matches = runtime.match(/(?<name>[a-zA-Z]+)?-?(?<version>[\d+.?]+)/);
+  if (matches?.groups) {
+    const name = matches.groups['name']
+    const version = parseVersion(matches.groups['version'], parseOpt)
+    return {
+      ...name && { name },
+      version
+    }
+  }
+
+  parseOpt.errorCallback('runtime', runtime, parseOpt.primaryKey);
+  return undefined;
+}
+
+function parseRaw(raw: string, parseOpt: ParseParam): Client | undefined {
+  const tokenize = raw.split('/')
+  let label: string | undefined
+  let version: Version | undefined
+  let os: OperatingSytem | undefined
+  let runtime: Runtime | undefined
+  
+  if (tokenize.length === 5) {
+    label = tokenize[1]
+    version = parseVersion(tokenize[2], parseOpt)
+    os = parseOs(tokenize[3], parseOpt)
+    runtime = parseRuntime(tokenize[4], parseOpt)
+  } else if (tokenize.length === 4) {
+    version = parseVersion(tokenize[1], parseOpt)
+    os = parseOs(tokenize[2], parseOpt)
+    runtime = parseRuntime(tokenize[3], parseOpt)
+  } else if (tokenize.length === 3) {
+    version = parseVersion(tokenize[0], parseOpt)
+    os = parseOs(tokenize[1], parseOpt)
+    runtime = parseRuntime(tokenize[2], parseOpt)
+  } else if (tokenize.length === 2) {
+    os = parseOs(tokenize[0], parseOpt)
+    runtime = parseRuntime(tokenize[1], parseOpt)
+  }
+
+  if (label || version || os || runtime) {
+    return {
+      ...label && { label },
+      ...version && { version },
+      ...os && { os },
+      ...runtime && { runtime },
+    };
+  }
+
+  parseOpt.errorCallback('raw', raw, parseOpt.primaryKey);
+  return undefined;
+}
+
 export function ClientsProcessor(
   data: ClientApiResponse[],
-  errorCallback: (error: string) => void
+  errorCallback: (entity: string, data: string, primaryKey: number) => void
 ) {
-  const parseDetail = (
-    clientId: string,
-    detail: string
-  ): ClientDetail | undefined => {
-    const mapping: { [key: string]: string } = {
-      linux: "vendor",
-      windows: "vendor",
-      darwin: "vendor",
-      x86: "architecture",
-      x64: "architecture",
-      amd64: "architecture"
-    };
 
-    const tryParseNumber = (input: string): number | undefined => parseInt(input) || undefined
+  const db = {
+    clients: []
+  }
 
-    const parseVersion = (version: string): Version | undefined => {
-      const matches = version.match(/v?(?<major>\d+)(?:.(?<minor>\d+).(?<patch>\d+)(?:-(?<tag>\w+)(?:-(?<build>[a-zA-Z0-9]+)(?:-(?<date>\d+))?)?)?)?/)
-      
-      if (!matches?.groups) {
-        errorCallback(`'Invalid Version: "${version}", for id: "${clientId}"`);
-        return undefined
-      }
+  let primaryKey = 0
 
-      const minor = tryParseNumber(matches.groups.minor)
-      const patch = tryParseNumber(matches.groups.patch)
-      const tag = matches.groups.tag
-      const build = matches.groups.build
-      const date = matches.groups.date
-
-      return {
-        major: parseInt(matches.groups.major),
-        ...minor && { minor },
-        ...patch && { patch },
-        ...tag && { tag },
-        ...build && { build },
-        ...date && { date },
-      }
-    };
-
-    const parseOs = (os: string): OperatingSytem | undefined => {
-      const match = matchAll(os, /(linux|windows|darwin|x86|x64|amd64)/g)
-      let matches = Array.from(match);
-
-      if (matches.length) {
-        let result: any = {};
-        matches.forEach((match) => {
-          const value = match[1];
-          const key = mapping[value];
-          result[key] = value;
-        });
-
-        return result;
-      }
-
-      errorCallback(`'Invalid OS: "${os}", for id: "${clientId}"`);
-      return undefined;
-    };
-
-    const parseRuntime = (runtime: string): Runtime | undefined => {
-      
-      let matches = runtime.match(/(?<name>[a-zA-Z]+)?-?(?<version>[\d+.?]+)/);
-      if (matches?.groups) {
-        let name = matches.groups['name']
-        let version = parseVersion(matches.groups['version'])
-        return {
-          ...name && { name },
-          version
-        }
-      }
-
-      errorCallback(`'Invalid Runtime: "${runtime}", for id: "${clientId}"`);
-      return undefined;
-    };
-
-    const tokenize = detail.toLowerCase().split('/')
-    let label: string | undefined
-    let version: Version | undefined
-    let os: OperatingSytem | undefined
-    let runtime: Runtime | undefined
-
-    if (tokenize.length === 4) {
-      label = tokenize[0]
-      version = parseVersion(tokenize[1])
-      os = parseOs(tokenize[2])
-      runtime = parseRuntime(tokenize[3])
-    } else if (tokenize.length === 3) {
-      version = parseVersion(tokenize[0])
-      os = parseOs(tokenize[1])
-      runtime = parseRuntime(tokenize[2])
-    } else if (tokenize.length === 2) {
-      os = parseOs(tokenize[0])
-      runtime = parseRuntime(tokenize[1])
-    }
-
-    if (label || version || os || runtime) {
-      return {
-        ...label && { label },
-        ...version && { version },
-        ...os && { os },
-        ...runtime && { runtime },
-      };
-    }
-
-    errorCallback(`'Invalid Detail: "${detail}", for id: "${clientId}"`);
-    return undefined;
-  };
-
-  const parseClient = (item: ClientApiResponse) => {
-    const matches = item.clientId.match(/(\w+)\/(.+)/);
-    if (matches) {
-      const rest = parseDetail(item.clientId, matches[2]);
-      if (!rest) {
+  const parse = (item: ClientApiResponse) => {
+    const matches = item.clientId.match(/(?<name>\w+)\/(?<raw>.+)/);
+    if (matches?.groups) {
+      const raw = parseRaw(item.clientId, { primaryKey, errorCallback });
+      if (!raw) {
         return undefined;
       }
 
       return {
-        id: matches[1],
-        ...rest,
+        name: matches.groups.name,
+        ...raw,
       };
     }
 
-    errorCallback(`'Invalid ClientId: "${item.clientId}"`);
+    errorCallback('clientId', item.clientId, primaryKey);
     return undefined;
   };
 
-  return data.map((item) => parseClient(item));
+  return data.map((item) => parse(item));
 }
