3689cfc67c3d954605e95a5f4ae15248d1af9105
Mohamed Mansour <hello@mohamedmansour.com>
2021-06-28 23:51:32 -0700

    Reuse same mock object for tests

--- src/data/DataProcessor.test.ts
+++ src/data/DataProcessor.test.ts
@@ -1,11 +1,13 @@
 import { ClientsProcessor } from "./DataProcessor";
 
-test("processes full schema correctly", () => {
+function mockProcessor() {
   const peers = [
     { clientId: "Geth/v1.10.3-stable-991384a7/linux-amd64/go1.16.3", count: 5 },
     { clientId: "Geth/v1.10.4-stable/linux-amd64/go1.16.4", count: 4 },
     { clientId: "Geth/goerli/v1.10.4-unstable-966ee3ae-20210528/linux-amd64/go1.16.4", count: 3 },
-    { clientId: "Geth/c30khkq4no1is608bj70/v1.10.4-stable-aa637fd3/linux-amd64/go1.16.5", count: 2 },
+    { clientId: "besu/v21.7.0-RC1/linux-x86_64/corretto-java-11", count: 14 },
+    { clientId: "erigon/v2021.06.5-alpha-a0694dd3/linux-amd64/go1.16.5", count: 1 },
+    { clientId: "OpenEthereum/v3.2.6-stable-f9f4926-20210514/x86_64-linux-gnu/rustc1.52.1", count: 1 },
   ];
    
   const errorCallback = jest.fn()
@@ -13,6 +15,11 @@ test("processes full schema correctly", () => {
   expect(processor).toHaveLength(peers.length)
   expect(errorCallback).not.toBeCalled()
 
+  return processor
+}
+
+test("processes full schema correctly", () => {
+  const processor = mockProcessor()
   expect(processor[0]).toEqual({
     count: 5,
     primaryKey: 1,
@@ -91,17 +98,11 @@ test("processes full schema correctly", () => {
 });
 
 test("processes runtimes correctly", () => {
-  const peers = [
-    { clientId: "besu/v21.7.0-RC1/linux-x86_64/corretto-java-11", count: 14 },
-    { clientId: "erigon/v2021.06.5-alpha-a0694dd3/linux-amd64/go1.16.5", count: 1 },
-    { clientId: "OpenEthereum/v3.2.6-stable-f9f4926-20210514/x86_64-linux-gnu/rustc1.52.1", count: 1 },
-  ];
-   
-  const errorCallback = jest.fn()
-  const processor = ClientsProcessor(peers, errorCallback).getRaw()
-  expect(processor).toHaveLength(peers.length)
-  expect(errorCallback).not.toBeCalled()
+  const processor = mockProcessor()
   expect(processor.map(p => (p?.runtime))).toEqual([
+    { name: 'go', version: { major: 1, minor: 16, patch: 3 } },
+    { name: 'go', version: { major: 1, minor: 16, patch: 4 } },
+    { name: 'go', version: { major: 1, minor: 16, patch: 4 } },
     { name: 'java', version: { major: 11 } },
     { name: 'go', version: { major: 1, minor: 16, patch: 5 } },
     { name: 'rustc', version: { major: 1, minor: 52, patch: 1 } }
@@ -109,22 +110,17 @@ test("processes runtimes correctly", () => {
 });
 
 test("processes os correctly", () => {
-  const peers = [
-    { clientId: "besu/v21.7.0-RC1/linux-amd64/corretto-java-11", count: 14 },
-    { clientId: "erigon/v2021.06.5-alpha-a0694dd3/windows/go1.16.5", count: 1 },
-    { clientId: "OpenEthereum/v3.2.6-stable-f9f4926-20210514/x86_64-linux-gnu/rustc1.52.1", count: 1 },
-  ];
-   
-  const errorCallback = jest.fn()
-  const processor = ClientsProcessor(peers, errorCallback).getRaw()
-  expect(processor).toHaveLength(peers.length)
-  expect(errorCallback).not.toBeCalled()
-  console.log(processor.map(p => (p?.os)))
+  const processor = mockProcessor()
   expect(processor.map(p => (p?.os))).toEqual([
-    { vendor: 'linux', architecture: 'amd64' },
-    { vendor: 'windows' },
-    { vendor: 'linux', architecture: 'x86_64' },
-  ])
+    {architecture: "amd64", vendor: "linux"}, 
+    {architecture: "amd64", vendor: "linux"}, 
+    {architecture: "amd64", vendor: "linux"}, 
+    {architecture: "x86_64", vendor: "linux"}, 
+    {architecture: "amd64", vendor: "linux"}, 
+    {architecture: "x86_64", vendor: "linux"}])
 });
 
-export {};
+test("searches for runtime", () => {
+  const processor = mockProcessor()
+
+})
