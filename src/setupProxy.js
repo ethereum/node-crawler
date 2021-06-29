e719cfc299ec0b4f507d1bdd7ecafdb19fbf2d5c
Mohamed Mansour <hello@mohamedmansour.com>
2021-06-28 20:00:15 -0700

    Add caching to client API, the API is always down

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,21 +1,59 @@
 const fetch = require('node-fetch');
+const fs = require('fs');
 
 let clients;
 
-async function cacheApiCall() {
-  const response = await fetch(process.env.CRAWLER_API_URL);
-  clients = await response.json()
+async function writeCache(data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./clients.json', data, 'utf8', (err) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
 }
 
-cacheApiCall()
+async function cacheCall() {
+  return new Promise((resolve, reject) => {
 
-module.exports = function(app) {
-  app.get('/rest/clients', (req, res) => {
-    if (!clients) {
-      return res.json({
-        status: 'loading'
-      })
+    fs.readFile('./clients.json', 'utf-8', (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(JSON.parse(data))
+      }
+    })
+  })
+}
+
+async function apiCall() {
+  
+  try {
+    const response = await fetch(process.env.CRAWLER_API_URL);
+    clients = await response.json()
+    if (clients && clients.length) {
+      await writeCache(clients)
+      return clients
+    } else {
+      console.error('Clients was empty, read from cache');
+      return cacheCall()
     }
-    return res.json(clients)
+  } catch (e) {
+    console.error('Tuweni API is down, read from cache');
+    return cacheCall()
   }
-)};
+}
+
+async function fetchClients() {
+  if (clients && clients.length) {
+    return clients
+  }
+
+  return apiCall()
+}
+
+module.exports = async (app) => {
+  app.get('/rest/clients', async (req, res) => res.json(await fetchClients()))
+};
