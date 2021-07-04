1a01cc804ff51b65e475626cf16f9a9824671597
Mohamed Mansour <hello@mohamedmansour.com>
2021-07-04 00:28:33 -0700

    UI: FIxed loader size

--- src/organisms/Loader.tsx
+++ src/organisms/Loader.tsx
@@ -8,7 +8,7 @@ interface LoaderProps {
 
 export function Loader(props: LoaderProps) {
   return (
-    <Center h="100vh" color="brand.primaryText">
+    <Center position="fixed" top={0} left={0} right={0} bottom={0} color="brand.primaryText">
       <VStack>
         <Logo width={100} height={100}/>
         <Text>{props.children}</Text>
